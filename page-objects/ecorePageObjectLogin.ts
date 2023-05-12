2e13041764cb0d0b00715b85f36e34e376d40291
Reuel B. Albuquerque Jr <reuelja@gmail.com>
2023-05-11 23:21:54 -0300

    validate the information exist and the text is the same at the Test Case: TC003

--- page-objects/ecorePageObjectLogin.ts
+++ page-objects/ecorePageObjectLogin.ts
@@ -1,4 +1,4 @@
-import {Locator, Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class ECorePageObjectLogin {
     readonly page: Page;
@@ -7,17 +7,64 @@ export class ECorePageObjectLogin {
     readonly btnLogin: Locator;
     readonly titleSucess: Locator;
     readonly msgWrongLogin: Locator;
-    readonly dataUser: [string, string, string];
-    readonly dataPassword: [string, string, string];
+    readonly DATA_USER: [string, string, string];
+    readonly DATA_PASSWORD: [string, string, string];
+    readonly firstLink: Locator;
 
-    constructor (page: Page){
-        this.btnLogin = page.getByRole('button', {name: 'Login'})
+    constructor(page: Page) {
+        this.btnLogin = page.getByRole('button', { name: 'Login' })
         this.getNameUser = page.getByTestId('username')
         this.getPasswordUser = page.getByTestId('password')
         this.titleSucess = page.getByText('Invoice List')
         this.msgWrongLogin = page.getByText('Wrong username or password.')
-        this.dataUser = ['Demouser', 'demouser_', 'demouser'];
-        this.dataPassword = ['abc123', 'xyz', 'nananana']
+        this.DATA_USER = ['Demouser', 'demouser_', 'demouser'];
+        this.DATA_PASSWORD = ['abc123', 'xyz', 'nananana']
+        this.firstLink = page.locator("//*[@href='/invoice/0']")
     }
-   
+
 }
+
+export class InvoiceDetails {
+    readonly page: Page;
+    readonly nameHotel: Locator;
+    readonly invoiceDate: Locator;
+    readonly duoDate: Locator;
+    readonly invoiceNumber: Locator;
+    readonly bookingCode: Locator;
+    readonly CUSTOMER_DETAILS: [string, string, string];
+    readonly customerDetailLine1: Locator;
+    readonly customerDetailLine2: Locator;
+    readonly customerDetailLine3: Locator;
+    readonly room: Locator;
+    readonly checkIn: Locator;
+    readonly checkOut: Locator;
+    readonly totalStayCount: Locator;
+    readonly totalStayAmount: Locator;
+    readonly depositNow: Locator;
+    readonly taxVat: Locator;
+    readonly totalAmount: Locator;
+
+    constructor(page: Page) {
+        this.nameHotel = page.getByText("Rendezvous Hotel");
+        this.invoiceDate = page.locator("/html/body/section/div/ul/li[1]/text()");
+        this.duoDate = page.locator("/html/body/section/div/ul/li[2]/text()");
+        this.invoiceNumber = page.getByText("Invoice #110 details");
+        this.bookingCode = page.getByText("Invoice #110 details");
+        this.customerDetailLine1 = page.locator("/html/body/section/div/div/text()[1]");
+        this.customerDetailLine2 = page.locator("/html/body/section/div/div/text()[2]");
+        this.customerDetailLine3 = page.locator("/html/body/section/div/div/text()[3]");
+        this.CUSTOMER_DETAILS = ["JOHNY SMITH", "R2, AVENUE DU MAROC", "123456"];
+        this.room = page.getByText("Superior Double");
+        this.checkIn = page.locator("/html/body/section/div/table[1]/tbody/tr[5]/td[2]");
+        this.checkOut = page.locator("/html/body/section/div/table[1]/tbody/tr[6]/td[2]");
+        this.totalStayCount = page.locator("/html/body/section/div/table[1]/tbody/tr[3]/td[2]");
+        this.totalStayAmount = page.locator("/html/body/section/div/table[1]/tbody/tr[4]/td[2]");
+        this.depositNow = page.locator("/html/body/section/div/table[2]/tbody/tr/td[1]");
+        this.taxVat = page.locator("/html/body/section/div/table[2]/tbody/tr/td[2]");
+        this.totalAmount = page.locator("/html/body/section/div/table[2]/tbody/tr/td[3]");
+
+
+
+    }
+}
+
--- tests/e-core-test.spec.ts
+++ tests/e-core-test.spec.ts
@@ -1,5 +1,5 @@
-import { test, expect } from '@playwright/test';
-import { ECorePageObjectLogin } from '../page-objects/ecorePageObjectLogin';
+import { test, expect, chromium } from '@playwright/test';
+import { ECorePageObjectLogin, InvoiceDetails } from '../page-objects/ecorePageObjectLogin';
 
 
 test.beforeEach(async ({ page }) => {
@@ -9,32 +9,81 @@ test.beforeEach(async ({ page }) => {
 test.describe('Test with Playwright for E-Core', ()=>{
     test('TC001 Login(Positive)', async({page}) =>{
         const ecore = new ECorePageObjectLogin(page);
-        await ecore.getNameUser.fill(ecore.dataUser[2]);
-        await ecore.getPasswordUser.fill(ecore.dataPassword[0]);
+        await ecore.getNameUser.fill(ecore.DATA_USER[2]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[0]);
         await ecore.btnLogin.click();
         await expect(ecore.titleSucess).toBeVisible();
     });
 
-    test('TC001 Login(Negative)', async({page}) =>{
+    test('TC002 Login(Negative)', async({page}) =>{
         const ecore = new ECorePageObjectLogin(page);
-        await ecore.getNameUser.fill(ecore.dataUser[0]);
-        await ecore.getPasswordUser.fill(ecore.dataPassword[0]);
+        await ecore.getNameUser.fill(ecore.DATA_USER[0]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[0]);
         await ecore.btnLogin.click();
         await expect(ecore.msgWrongLogin).toBeVisible();
 
-        await ecore.getNameUser.fill(ecore.dataUser[1]);
-        await ecore.getPasswordUser.fill(ecore.dataPassword[1]);
+        await ecore.getNameUser.fill(ecore.DATA_USER[1]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[1]);
         await ecore.btnLogin.click();
         await expect(ecore.msgWrongLogin).toBeVisible();
 
-        await ecore.getNameUser.fill(ecore.dataUser[2]);
-        await ecore.getPasswordUser.fill(ecore.dataPassword[2]);
+        await ecore.getNameUser.fill(ecore.DATA_USER[2]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[2]);
         await ecore.btnLogin.click();
         await expect(ecore.msgWrongLogin).toBeVisible();
 
-        await ecore.getNameUser.fill(ecore.dataUser[2]);
-        await ecore.getPasswordUser.fill(ecore.dataPassword[0]);
+        await ecore.getNameUser.fill(ecore.DATA_USER[2]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[0]);
         await ecore.btnLogin.click();
         await expect(ecore.msgWrongLogin).toBeVisible();
     });
-})
+
+    test('TC003 Validade invoice details', async({page}) =>{
+        const ecore = new ECorePageObjectLogin(page);
+        const pageDetails = new InvoiceDetails(page);
+        await ecore.getNameUser.fill(ecore.DATA_USER[2]);
+        await ecore.getPasswordUser.fill(ecore.DATA_PASSWORD[0]);
+        await ecore.btnLogin.click();
+        await ecore.firstLink.click();
+
+        // select other tab
+
+        //Validate if visible elements
+        await expect(pageDetails.nameHotel).toBeVisible();
+        await expect(pageDetails.invoiceDate).toBeVisible();
+        await expect(pageDetails.duoDate).toBeVisible();
+        await expect(pageDetails.invoiceNumber).toBeVisible();
+        await expect(pageDetails.bookingCode).toBeVisible();
+        await expect(pageDetails.customerDetailLine1).toBeVisible();
+        await expect(pageDetails.customerDetailLine2).toBeVisible();
+        await expect(pageDetails.customerDetailLine3).toBeVisible();
+        await expect(pageDetails.room).toBeVisible();
+        await expect(pageDetails.checkIn).toBeVisible();
+        await expect(pageDetails.checkOut).toBeVisible();
+        await expect(pageDetails.totalStayCount).toBeVisible();
+        await expect(pageDetails.totalStayAmount).toBeVisible();
+        await expect(pageDetails.depositNow).toBeVisible();
+        await expect(pageDetails.taxVat).toBeVisible();
+        await expect(pageDetails.totalAmount).toBeVisible();
+
+        //validade text elements
+        await expect(pageDetails.nameHotel).toHaveText('Rendezvous Hotel');
+        await expect(pageDetails.invoiceDate).toHaveText('14/01/2018');
+        await expect(pageDetails.duoDate).toHaveText('15/01/2018');
+        await expect(pageDetails.invoiceNumber).toHaveText('Invoice #110 details');
+        await expect(pageDetails.bookingCode).toHaveText('0875');
+        await expect(pageDetails.customerDetailLine1).toHaveText(pageDetails.CUSTOMER_DETAILS[0]);
+        await expect(pageDetails.customerDetailLine2).toHaveText(pageDetails.CUSTOMER_DETAILS[1]);
+        await expect(pageDetails.customerDetailLine3).toHaveText(pageDetails.CUSTOMER_DETAILS[2]);
+        await expect(pageDetails.room).toHaveText('Superior Double');
+        await expect(pageDetails.checkIn).toHaveText('14/01/2018');;
+        await expect(pageDetails.checkOut).toHaveText('15/01/2018');
+        await expect(pageDetails.totalStayCount).toHaveText('1');
+        await expect(pageDetails.totalStayAmount).toHaveText('$150');
+        await expect(pageDetails.depositNow).toHaveText('USD $20.90');
+        await expect(pageDetails.taxVat).toHaveText('USD $19');
+        await expect(pageDetails.totalAmount).toHaveText('USD $209');
+     
+
+    });
+});
